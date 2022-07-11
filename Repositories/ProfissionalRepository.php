a8c1b09cf2517981f7ba67695b67ac24fddc5dca
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-07-11 16:55:02 -0300

    add options consulta mongodb

--- Repositories/ProfissionalRepository.php
+++ Repositories/ProfissionalRepository.php
@@ -6,11 +6,11 @@ use CNESIntegration\Connection\Conn;
 
 class ProfissionalRepository extends Repository
 {
-    public function getProfissionais($filter = [])
+    public function getProfissionais($filter = [], $options = [])
     {
         $manager = Conn::getManager();
+        $query = new \MongoDB\Driver\Query($filter, $options);
 
-        $query = new \MongoDB\Driver\Query($filter);
         $result = $manager->executeQuery($this->database .'.'. $this->collection, $query);
 
         return $result;
