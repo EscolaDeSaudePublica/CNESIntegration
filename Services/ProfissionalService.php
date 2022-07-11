f50d3fca828854b40adf3b0412df294b3040090e
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-07-11 15:44:04 -0300

    Implementa atualização dos profissionais (remove vinculos e adiciona os novos vigentes)

--- Services/ProfissionalService.php
+++ Services/ProfissionalService.php
@@ -3,10 +3,11 @@
 namespace CNESIntegration\Services;
 
 use CNESIntegration\Repositories\ProfissionalRepository;
+use CNESIntegration\Repositories\SpaceRepository;
 use MapasCulturais\App;
 use MapasCulturais\Entities\Agent;
 
-class ProfissionalService 
+class ProfissionalService
 {
     public function atualizaProfissionais()
     {
@@ -20,7 +21,7 @@ class ProfissionalService
 
 
         $filter = [
-            'CNS' => 706600584508210
+            //'CNS' => 700507591592556
         ];
 
         /**
@@ -28,53 +29,74 @@ class ProfissionalService
          */
         $ProfissionalRepository = new ProfissionalRepository();
         $profissionais = $ProfissionalRepository->getProfissionais($filter);
-
         $i = 1;
         foreach ($profissionais as $profissional) {
             $cns = (int) $profissional->CNS;
-            $cbo = $profissional->CBO . ' - ' . $profissional->{'DESCRICAO CBO'};
             $cnes = $profissional->CNES;
-            $nome = $profissional->NOME;
-            
+
+            $spaceRepository = new SpaceRepository();
+            $spaceMeta = $spaceRepository->getSpacesMetaByCNES($cnes);
+
+            if ($spaceMeta) {
+                $space = $spaceMeta->owner;
+            } else {
+                // TODO: Caso o espaço não exista, deve ser adicionado o espaço e em seguida continua o fluxo
+            }
+
             // busca no banco do mapa se o CNS está cadastrado, ou seja, se o profissional já foi migrado anteriomente
             $agentMeta = $app->repo('AgentMeta')->findOneBy(['value' => $cns]);
 
-            // se existir o agente, então deve sexistir a rotina de atualização do profissional
+            // se existir o agente, então deve existir a rotina de atualização do profissional
             if ($agentMeta) {
                 $agent = $agentMeta->owner;
 
                 // retorna as relações com espaços do agente
                 $conn = $app->em->getConnection();
                 $relations = $conn->fetchAll("
-                    SELECT * FROM agent_relation 
+                    SELECT id FROM agent_relation 
                     WHERE agent_id = {$agent->id} 
                     AND object_type = 'MapasCulturais\Entities\Space'
-                    AND type LIKE '%{$profissional->CBO}%'
                 ");
                 foreach ($relations as $relation) {
                     // realiza a limpeza dos relacionamentos dos agentes com os espaços
                     $relation_ = $app->repo('AgentRelation')->find($relation['id']);
                     $relation_->delete(true);
+                    echo "Removendo vinculos do agente {$agent->id}<br>";
                 }
-           
-                // Busca o espaço para adicionar um novo relation com o agent
-                // $spaceMeta = $app->repo('SpaceMeta')->findOneBy(['value' => $cnes]);
-                $query = $app->em->createQuery("SELECT s FROM MapasCulturais\Entities\SpaceMeta s WHERE s.value LIKE :value");
+            }
 
-                $query->setParameters([
-                    "value" => "%{$cnes}%"
-                ]);
+            if ($i++ == 50) {
+                break;
+            }
+        }
 
-                $spaceMeta = $query->getOneOrNullResult();
+        echo '<hr><hr><br>';
 
-                if ($spaceMeta) {
-                    $space = $spaceMeta->owner;
+        $i = 1;
+        $profissionais_ = $ProfissionalRepository->getProfissionais($filter);
+        foreach ($profissionais_ as $profissional_) {
+            $cns = (int) $profissional_->CNS;
+            $cbo = $profissional_->CBO . ' - ' . $profissional_->{'DESCRICAO CBO'};
+            $cnes = $profissional_->CNES;
+            $nome = $profissional_->NOME;
 
-                    // adiciona o relacionamento do espaço com o agente retornado do mongodb, concatenando com o CBO (id do cbo + descrição do cbo)
-                    $space->createAgentRelation($agent, $cbo);     
-                } else {
-                    // TODO: Caso o espaço não exista, deve ser adicionado o espaço e em seguida continua o fluxo
-                }
+            $agentMeta = $app->repo('AgentMeta')->findOneBy(['value' => $cns]);
+
+            $spaceRepository = new SpaceRepository();
+            $spaceMeta = $spaceRepository->getSpacesMetaByCNES($cnes);
+
+            if ($spaceMeta) {
+                $space = $spaceMeta->owner;
+            }
+
+            // se existir o agente, então deve existir a rotina de atualização do profissional
+            if ($agentMeta) {
+                $agent = $agentMeta->owner;
+
+                // adiciona o relacionamento do espaço com o agente retornado do mongodb, concatenando com o CBO (id do cbo + descrição do cbo)
+                $space->createAgentRelation($agent, $cbo);
+                echo "Add vinculo existente do agent {$agent->id} e vinculando ao espaço {$space->id} com CBO: {$cbo}<br>";
+
             } else {
                 // TODO: se não existir o agente, então deve existir a rotina de cadastro do profissional
                 $agent = new \MapasCulturais\Entities\Agent;
@@ -84,15 +106,20 @@ class ProfissionalService
                 $agent->shortDescription = "CNS: {$cns}";
                 $agent->setMetadata('cns', $cns);
                 $agent->save(true);
+                echo "Novo agente {$agent->id}";
 
+
+                // adiciona o relacionamento do espaço com o agente retornado do mongodb, concatenando com o CBO (id do cbo + descrição do cbo)
+                $space->createAgentRelation($agent, $cbo);
+                echo "Add vinculo do novo agent {$agent->id} e vinculando ao espaço {$space->id} com CBO: {$cbo}<br>";
             }
             
 
             /**
              * executa apenas 1 vez dentro do foreach
              */
-            if ($i++ == 10) {
-                die;
+            if ($i++ == 50) {
+                break;
             }
         } 
     }
