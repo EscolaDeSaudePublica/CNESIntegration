40a465975938197735461c48c3ddd93b5b9d748a
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-10-27 11:14:11 -0300

    remove limit

--- Repositories/EstabelecimentoDWRepository.php
+++ Repositories/EstabelecimentoDWRepository.php
@@ -30,7 +30,7 @@ class EstabelecimentoDWRepository
     {
         $sql = "SELECT distinct competencia, co_cnes, no_razao_social, no_fantasia, no_logradouro, nu_endereco, no_complemento, no_bairro, co_cep, dt_atualizacao, 
         nu_latitude, nu_longitude, tp_unidade, description, atende_sus, co_municipio_gestor, municipio, nu_telefone 
-        FROM estabelecimentos LIMIT 50";
+        FROM estabelecimentos";
 
         $sth = $this->connection->prepare($sql);
         $sth->execute();
