0f77ea92601c27e103986971a3f0c1ae0dcb7dd2
victorMagalhaesPacheco <victor.magalhaesp@gmail.com>
2022-11-07 11:48:56 -0300

    Add envio de e-mail inicio e fim migração

--- cron.php
+++ cron.php
@@ -8,6 +8,20 @@ use MapasCulturais\App;
 
 $app = App::i();
 
+$app->createAndSendMailMessage([
+    'from' => $app->config['mailer.from'],
+    'to' => 'victor.magalhaesp@gmail.com',
+    'subject' => 'MIGRAÇÃO CNES - INICIANDO',
+    'body' => 'MIGRAÇÃO CNES - INICIANDO'
+]);
+
 $plugins = $app->getPlugins();
 $cnesIntegration = $plugins['CNESIntegration'];
 $cnesIntegration->runMigrationCNES();
+
+$app->createAndSendMailMessage([
+    'from' => $app->config['mailer.from'],
+    'to' => 'victor.magalhaesp@gmail.com',
+    'subject' => 'MIGRAÇÃO CNES - FINALIZADO',
+    'body' => 'MIGRAÇÃO CNES - FINALIZADO'
+]);
